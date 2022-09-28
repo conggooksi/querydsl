d47228649b80c67553b30e98de09bfd911c488a9
kuki <kimgeonkuk@gmail.com>
2022-09-28 17:57:34 +0900

    until useBuilder

--- src/main/java/study/querydsl/QuerydslApplication.java
+++ src/main/java/study/querydsl/QuerydslApplication.java
@@ -2,7 +2,6 @@ package study.querydsl;
 
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
-import springfox.documentation.swagger2.annotations.EnableSwagger2;
 
 @SpringBootApplication
 public class QuerydslApplication {
